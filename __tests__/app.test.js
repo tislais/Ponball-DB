39109bcad150731399ef3412ed352e275ce13530
Tis Lais <tistististis@gmail.com>
2021-07-01 20:02:23 -0700

    Add failing get test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,26 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import ingest from '../lib/ingest.js';
+import processPage from '../lib/process.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('gets name, date, and manufacturer via GET', async () => {
+
+    const actual = await ingest('https://www.ipdb.org/machine.cgi?id=6573')
+      .then(html => processPage(html));
+
+    const expected = {
+      name: 'Jurassic Park (Pro)',
+      manufacturer: 'Stern Pinball',
+      date: 'August, 2019'
+    };
+    
+    expect(actual).toEqual(expected);
+  });
+
 });
--- data/setup.js
+++ data/setup.js
@@ -1,18 +1,6 @@
 import { promises as fs } from 'fs';
-import path from 'path';
 
 export default (pool) => {
-  return fs
-    .readFile(
-      path.join(
-        path.dirname(new URL(import.meta.url).pathname),
-        '..',
-        'sql',
-        'setup.sql'
-      ),
-      {
-        encoding: 'utf-8',
-      }
-    )
-    .then((sql) => pool.query(sql));
+  return fs.readFile('./sql/setup.sql', { encoding: 'utf-8' })
+    .then(sql => pool.query(sql));
 };

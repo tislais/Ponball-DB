16f1d21eee945625444aff83edddd001f36381cb
Tis Lais <tistististis@gmail.com>
2021-09-10 13:03:13 -0700

    Add condition for pure mechanical type

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ import ingest from './lib/ingest.js';
 import processPage from './lib/process.js';
 import store from './lib/store.js';
 import dotenv from 'dotenv';
+import pool from './lib/utils/pool.js';
+import setup from './data/setup.js';
 
 dotenv.config();
+setup(pool);
 
-ingest('https://www.ipdb.org/machine.cgi?id=6575')
-  .then(html => processPage(html))
-  .then(machines => {
-    console.log(machines);
-    store(machines);
-  });
+[...Array(50)].map((_, i) => {
+  ingest(`https://www.ipdb.org/machine.cgi?id=${i + 1}`)
+    .then(html => processPage(html))
+    .then(machines => {
+      console.log(machines);
+      store(machines);
+    });
+});
+
+
+

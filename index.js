bd41062bb0e57ebadafaf1697a381531513ea46b
Tis Lais <tistististis@gmail.com>
2021-09-10 16:20:09 -0700

    Scrape for mpu

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import setup from './data/setup.js';
 dotenv.config();
 setup(pool);
 
-[...Array(5)].map((_, i) => {
+[...Array(1)].map((_, i) => {
   setTimeout (() => {
     ingest(`https://www.ipdb.org/machine.cgi?id=${i + 1}`)
       .then(html => processPage(html))
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,9 +3,10 @@ DROP TABLE IF EXISTS machines;
 CREATE TABLE machines (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   ipdb_id BIGINT,
-  title TEXT NOT NULL,
-  type TEXT NOT NULL,
-  manufacturer TEXT NOT NULL,
-  manufacture_date TEXT NOT NULL,
-  production TEXT
+  title TEXT,
+  type TEXT,
+  manufacturer TEXT,
+  manufacture_date TEXT,
+  production TEXT,
+  mpu TEXT
 )

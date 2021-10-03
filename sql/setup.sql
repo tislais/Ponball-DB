58bf27478148b6a1c5390487745f790ccc2ead2c
Tis Lais <tistististis@gmail.com>
2021-10-03 14:36:37 -0700

    Separate mpu data to mpu table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,13 +1,19 @@
 DROP TABLE IF EXISTS machine;
 DROP TABLE IF EXISTS label;
 DROP TABLE IF EXISTS manufacturer;
+DROP TABLE IF EXISTS mpu;
 
 CREATE TABLE manufacturer (
   manufacturer_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name TEXT,
-  trade_name TEXT,
-  first_year INT4,
-  last_year INT4
+  manufacturer_name TEXT,
+  manufacturer_trade_name TEXT,
+  manufacturer_first_year INT4,
+  manufacturer_last_year INT4
+);
+
+CREATE TABLE mpu (
+  mpu_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT
 );
 
 CREATE TABLE machine (
@@ -17,8 +23,8 @@ CREATE TABLE machine (
   type TEXT,
   manufacturer_id BIGINT,
   manufacture_date TEXT,
-  production TEXT,
-  mpu TEXT,
+  production BIGINT,
+  mpu_id BIGINT,
   theme TEXT,
   specialty TEXT,
   features TEXT,
@@ -32,7 +38,8 @@ CREATE TABLE machine (
   rule_sheets JSONB[],
   roms JSONB[],
   docs JSON[],
-  CONSTRAINT fk_manufacturer FOREIGN KEY(manufacturer_id) REFERENCES manufacturer(manufacturer_id)
+  CONSTRAINT fk_manufacturer FOREIGN KEY(manufacturer_id) REFERENCES manufacturer(manufacturer_id),
+  CONSTRAINT fk_mpu FOREIGN KEY(mpu_id) REFERENCES mpu(mpu_id)
 );
 
 CREATE TABLE label (

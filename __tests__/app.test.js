1d9d932b691597dbc9b019e0c7fa907de239d2fe
Tis Lais <tistististis@gmail.com>
2022-01-22 15:21:36 -0800

    Fix labels array in process

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,8 +10,9 @@ describe('demo routes', () => {
   });
 
   it('gets name, date, and manufacturer via GET', async () => {
-    const actual = await ingest('https://www.ipdb.org/machine.cgi?id=2845')
-      .then(html => processPage(html));
+    const actual = await ingest(
+      'https://www.ipdb.org/machine.cgi?id=2845'
+    ).then((html) => processPage(html));
     const expected = machineStub;
     expect(actual).toEqual(expected);
   });
